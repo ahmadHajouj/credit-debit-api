66786038e6d3565bbafc2f1cceb2d2768dc88e5e
ahmadHajouj <ahmad1416h@gmail.com>
2023-08-14 07:20:17 +0300

    access thing

--- index.js
+++ index.js
@@ -29,10 +29,8 @@ app.use(express.json());
 
 app.use(function (req, res, next) {
   res.header("Access-Control-Allow-Origin", "*");
-  res.header(
-    "Access-Control-Allow-Headers",
-    "Origin, X-Requested-With, Content-Type, Accept"
-  );
+  res.header("Access-Control-Allow-Methods", "POST, GET, PUT");
+  res.header("Access-Control-Allow-Headers", "Content-type");
   next();
 });
 
