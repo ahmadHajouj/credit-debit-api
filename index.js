f9262366f02f9af9910b498f0847019e807f417a
ahmadHajouj <ahmad1416h@gmail.com>
2023-08-14 23:17:21 +0300

    methods

--- index.js
+++ index.js
@@ -23,6 +23,7 @@ app.use(express.json());
 app.use(
   cors({
     origin: ["https://credit-debit-app.vercel.app", "*"],
+    methods: ["GET", "POST", "PUT", "OPTIONS"],
     headers: ["Content-Type"],
     credentials: true,
   })
